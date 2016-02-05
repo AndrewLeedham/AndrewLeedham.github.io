8f024a9851bcf47d6f333427ce3023ef5b4d72e2
Andrew Leedham <Andrew Leedham>
2016-02-05 00:27:50 +0000

    Started work on the portfolio section

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 
   $("a[href*='#']").click(function(event){
     $("html, body").animate({
-      scrollTop: $(this.hash).offset().top - $("nav.fixed").outerHeight()
+      scrollTop: $(this.hash).offset().top - $("nav").outerHeight()
     });
     event.preventDefault();
   });
