5152b3ee6a6da927e6f5b2a6f32d4c812d9dbc29
Andrew Leedham <Andrew Leedham>
2016-02-11 13:00:12 +0000

    Add content and styling to blog archive, Changed the way landing optinality works

--- js/script.js
+++ js/script.js
@@ -1,27 +1,8 @@
 $(document).ready(function(){
-
-
-
-  function scroll(){
-    if($(document).scrollTop() > 0)
-    {
-      $("nav").addClass("fixed");
-    }else{
-      $("nav").removeClass("fixed");
-    }
-  }
-
-  scroll();
-
-  $(document).scroll(function(){
-    scroll();
-  });
-
   $("a[href*='#']").click(function(event){
     $("html, body").animate({
       scrollTop: $(this.hash).offset().top - $("nav").outerHeight()
     });
     event.preventDefault();
   });
-
 });
