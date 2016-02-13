1be366f7990289247c0f1b64520699a83ba3b99e
Andrew Leedham <AndrewLeedham@Outlook.com>
2016-02-13 21:29:30 +0000

    Add photoshoot zoom & project title

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@ $(document).ready(function(){
   });
 
   $(window).resize(function(){
-    console.log($("body").outerWidth());
     if($(document).outerWidth() > 500){
       $("body").css({
         left: "",
@@ -84,4 +83,32 @@ $(document).ready(function(){
       });
     }
   });
+
+  $("div.photo").click(function(event){
+    $(".fullscreen-photo").fadeIn().append($(this).clone().css({
+      position: "fixed",
+      top: $(this).offset().top - $(document).scrollTop(),
+      left: $(this).offset().left - $("body").offset().left,
+      width: $(this).outerWidth(),
+      height: $(this).outerHeight()
+    }).animate({
+      left: 0,
+      top: 0,
+      width: $(window).outerWidth(),
+      height: $(window).outerHeight()
+    }, {
+      complete: function(){
+        $(this).css({
+          width: "100%",
+          height: "100%"
+        });
+      }
+    }));
+    event.preventDefault();
+  });
+  $(".fullscreen-photo").click(function(){
+    $(this).empty();
+    $(this).fadeOut();
+  });
+
 });
