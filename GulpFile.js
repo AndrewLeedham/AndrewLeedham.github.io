2d9f617e8f0944386f0e2915474c2558c32cbe88
Andrew Leedham <AndrewLeedham@outlook.com>
2017-02-04 21:05:41 +0000

    Added browser sync

--- GulpFile.js
+++ GulpFile.js
@@ -1,10 +1,18 @@
 var gulp = require("gulp"),
     spawn = require("child_process").spawn,
-    plugins = require("gulp-load-plugins")();
+    plugins = require("gulp-load-plugins")(),
+    sync = require("browser-sync").create();
 
-gulp.task("default", ["watch"]);
+gulp.task("default", ["watch", "sass", "jekyll"]);
 
-gulp.task("watch", ["sass", "jekyll"], function(){
+gulp.task("watch", function(){
+    sync.init({
+        files: ["./_site/**"],
+        port: 4000,
+        server: {
+            baseDir: "./_site/"
+        }
+    });
     gulp.watch("./_sass/**/*.{sass,scss}", ["sass"]);
 });
 
@@ -15,14 +23,16 @@ gulp.task("sass", function(){
 });
 
 gulp.task("jekyll", function(cb){
-    var jekyll = spawn("jekyll.bat", ["serve", "--watch"]);
+    var jekyll = spawn("jekyll.bat", ["serve", "--skip-initial-build", "--incremental", "--quiet"]);
 
-    var log = function(buffer){
+    var log = function(buffer, post){
         buffer.toString().split(/\n/).forEach(function(message){
-            plugins.util.log('Jekyll: ' + message);
+            if(!/\S/.test(message)){
+                plugins.util.log("Jekyll-" + post + ": " + message);
+            }
         });
     }
 
-    jekyll.stdout.on("data", log);
-    jekyll.stderr.on("data", log);
+    jekyll.stdout.on("data", function(buffer){log(buffer, "out")});
+    jekyll.stderr.on("data", function(buffer){log(buffer, "err")});
 });
