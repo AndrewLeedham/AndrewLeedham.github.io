af8958076976c9e835656920c700db7acc67b2d7
Andrew Leedham <AndrewLeedham@outlook.com>
2017-02-20 19:26:28 +0000

    Update to gulp 4.0.
    Seperate watch and build tasks.
    Add clean task.
    Convert GulpFile.js structure to 4.0 parallel/series

--- GulpFile.js
+++ GulpFile.js
@@ -1,53 +1,37 @@
 var gulp = require("gulp"),
-    spawn = require("child_process").spawn,
+    exec = require("child_process").exec,
     plugins = require("gulp-load-plugins")(),
     sync = require("browser-sync").create(),
     webp = require("imagemin-webp"),
-    path = require("path");
+    path = require("path"),
+    del = require('del');
 
 var SASS_INCLUDES = [
     path.join(__dirname, "/node_modules/bourbon-neat/core/"),
     path.join(__dirname, "/node_modules/bourbon/app/assets/stylesheets/")
 ]
 
-console.log(SASS_INCLUDES);
-
-gulp.task("default", ["watch", "sass", "img", "jekyll", "favicons", "js"]);
-
-gulp.task("watch", function(){
-    sync.init({
-        files: ["./source/_site/**"],
-        port: 4000,
-        server: {
-            baseDir: "./dest/"
-        }
-    });
-    gulp.watch("./source/_sass/**/*.{sass,scss}", ["sass"]);
-    gulp.watch("./source/_img/**/*.jpg", ["jpg"]);
-    gulp.watch("./source/_js/**/*.js", ["js"]);
-});
-
-gulp.task("sass", function(){
+function sass(){
     return gulp.src("./source/_sass/**/*.{sass,scss}")
         .pipe(plugins.sass({includePaths: SASS_INCLUDES}))
         .pipe(plugins.autoprefixer({
             cascade: false
         }))
         .pipe(gulp.dest("./source/css/"));
-});
+};
 
-gulp.task("img", ["jpg"/*, "png", "svg"*/]);
-
-gulp.task("jpg", function(){
+function jpg(){
     return gulp.src("./source/_img/**/*.jpg")
     .pipe(plugins.imagemin([ plugins.imagemin.jpegtran({progressive: true}) ]))
     .pipe(gulp.dest("./source/img/"))
     .pipe(plugins.imagemin([ webp({quality: 80}) ]))
     .pipe(plugins.rename({extname: ".webp"}))
     .pipe(gulp.dest("./source/img/"));
-});
+};
 
-gulp.task("favicons", function(cb){
+var img = gulp.parallel(jpg/*, "png", "svg"*/);
+
+function favicons(){
     return gulp.src("./source/_img/logo.svg")
     .pipe(plugins.favicons({
         appName: "Andrew Leedham's Portfolio",
@@ -67,19 +51,23 @@ gulp.task("favicons", function(cb){
         replace: false
     }))
     .pipe(gulp.dest("./source/"));
-});
+};
 
-gulp.task("js", function(cb){
+function js(){
     return gulp.src("./source/_js/**/*.js")
     .pipe(plugins.uglify())
     .pipe(plugins.concatUtil("script.js"))
     .pipe(plugins.concatUtil.header("(function(){'use scrict';"))
     .pipe(plugins.concatUtil.footer("})();"))
     .pipe(gulp.dest("./source/js/"));
-});
+};
 
-gulp.task("jekyll", function(cb){
-    var jekyll = spawn("jekyll.bat", ["serve", "--skip-initial-build", "--incremental", "--quiet"]);
+function clean(){
+    return del(["./dest/**/*"]);
+}
+
+function jekyll(done){
+    var jekyll = exec("jekyll.bat serve --skip-initial-build --incremental --quiet", done);
 
     var log = function(buffer, post){
         buffer.toString().split(/\n/).forEach(function(message){
@@ -91,4 +79,38 @@ gulp.task("jekyll", function(cb){
 
     jekyll.stdout.on("data", function(buffer){log(buffer, "out")});
     jekyll.stderr.on("data", function(buffer){log(buffer, "err")});
+};
+
+function jekyllBuild(done){
+    var jekyll = exec("jekyll.bat build", done);
+
+    var log = function(buffer, post){
+        buffer.toString().split(/\n/).forEach(function(message){
+            if(/\S/.test(message)){
+                plugins.util.log("Jekyll-" + post + ": " + message);
+            }
+        });
+    }
+
+    jekyll.stdout.on("data", function(buffer){log(buffer, "out")});
+    jekyll.stderr.on("data", function(buffer){log(buffer, "err")});
+};
+
+var watch = gulp.parallel(jekyll, function(){
+    sync.init({
+        files: ["./source/_site/**"],
+        port: 4000,
+        server: {
+            baseDir: "./dest/"
+        }
+    });
+    gulp.watch("./source/_sass/**/*.{sass,scss}", sass);
+    gulp.watch("./source/_img/**/*.jpg", jpg);
+    gulp.watch("./source/_js/**/*.js", js);
 });
+
+gulp.task("default", watch);
+
+gulp.task("build", gulp.series(clean, gulp.parallel(sass, img, jekyllBuild, favicons, js)));
+
+gulp.task("watch", watch);
