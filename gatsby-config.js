1de66d16a254a0f40cd85d7a04faa587aea9d352
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-05-17 21:57:14 +0100

    Enabled service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,9 +71,7 @@ module.exports = {
           include: /\.svg$/
         }
       }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    },
+    `gatsby-plugin-offline`
   ]
 };
